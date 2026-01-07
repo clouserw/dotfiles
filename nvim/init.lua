a983d40f3c6d7878a26dd28050c0231749cd511a
Wil Clouser <clouserw@micropipes.com>
2026-01-06 16:31:59 -0800

    ctrl-hjkl bindings

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.keymap.set("n", "<C-J>", "<C-W>j<C-W>_", { silent = true })
+vim.keymap.set("n", "<C-K>", "<C-W>k<C-W>_", { silent = true })
+vim.keymap.set("n", "<C-H>", "<C-W>h<C-W>|", { silent = true })
+vim.keymap.set("n", "<C-L>", "<C-W>l<C-W>|", { silent = true })
